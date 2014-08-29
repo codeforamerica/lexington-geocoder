f290c10a163c7fc2d8321ebfaec4130487b05800
Lyzi Diamond <lyzidiamond@gmail.com>
2014-08-29 15:02:28 -0700

    Work in progress: create map object

--- public/js/map.js
+++ public/js/map.js
@@ -1,14 +1,33 @@
+// Create map object
+var mapObject = {};
+
 // initialize map
-var map = L.mapbox.map('map', 'codeforamerica.hek4o94g', {maxZoom: 15, minZoom: 10, accessToken: 'pk.eyJ1IjoiY29kZWZvcmFtZXJpY2EiLCJhIjoiSTZlTTZTcyJ9.3aSlHLNzvsTwK-CYfZsG_Q'}).setView([38.042,-84.515], 11);
+mapObject.init = function() {
+  // Create map container
+  this.map = L.mapbox.map('map', 'codeforamerica.hek4o94g', {maxZoom: 15, minZoom: 10, accessToken: 'pk.eyJ1IjoiY29kZWZvcmFtZXJpY2EiLCJhIjoiSTZlTTZTcyJ9.3aSlHLNzvsTwK-CYfZsG_Q'}).setView([38.042,-84.515], 11);
 
-var addresses = L.layerGroup();
+  // Make layer group
+  this.addresses = L.layerGroup();
 
-function addToMap(data) {
-  addresses.clearLayers();
+}
+
+// add search results to map
+mapObject.addToMap = function (data) {
+  this.addresses.clearLayers();
   var geoJsonResults = L.geoJson(data, {
     onEachFeature: function(feature, layer) {
       layer.bindPopup(feature.properties.formatted_address);
     }
   });
-  addresses.addLayer(geoJsonResults).addTo(map);
+  this.addresses.addLayer(geoJsonResults).addTo(this.map);
 }
+
+// Creates a Leaflet map object
+// Sets options for map object
+// Sets starting view for map
+// Creates addresses layerGroup
+
+// Clears layers from addresses
+// Creates Leaflet geojson object of results
+// Adds a popup to each results
+// Adds results to addresses
